ebc14a7cea4d925dc6d1cec3c73fafe865211a52
r0fus0d <d2hwakh7bs5e@protonmail.com>
2023-10-07 17:26:30 +0800

    update build CI

--- main.go
+++ main.go
@@ -2,20 +2,20 @@ package main
 
 import (
 	"context"
-    "flag"
+	"flag"
 	"fmt"
+	"golang.org/x/sys/unix"
 	"net"
 	"os"
-	"syscall"
 	"strings"
-    "golang.org/x/sys/unix"
+	"syscall"
 )
 
 var lc = net.ListenConfig{
 	Control: func(network, address string, c syscall.RawConn) error {
 		var opErr error
 		if err := c.Control(func(fd uintptr) {
-		   opErr = unix.SetsockoptInt(int(fd), unix.SOL_SOCKET, unix.SO_REUSEADDR, 1)
+			opErr = unix.SetsockoptInt(int(fd), unix.SOL_SOCKET, unix.SO_REUSEADDR, 1)
 			opErr = unix.SetsockoptInt(int(fd), unix.SOL_SOCKET, unix.SO_REUSEPORT, 1)
 		}); err != nil {
 			return err
@@ -58,18 +58,18 @@ func forward(src, dst net.Conn) {
 
 func main() {
 	pid := os.Getpid()
-    // 定义命令行参数
-    var localPort string
-    var remoteAddr string
-    var sourceAddr string
-    var listenAddr string
+	// 定义命令行参数
+	var localPort string
+	var remoteAddr string
+	var sourceAddr string
+	var listenAddr string
 
-    // 解析命令行参数
-    flag.StringVar(&localPort, "listenport", "80", "local port to listen")
-    flag.StringVar(&remoteAddr, "remoteaddr", "2.2.2.2:22", "remote address to forward")
-    flag.StringVar(&sourceAddr, "sourceaddr", "1.1.1.1", "source address to filter")
-    flag.StringVar(&listenAddr, "listenaddr", "172.19.0.2", "local address to listen")
-    flag.Parse()
+	// 解析命令行参数
+	flag.StringVar(&localPort, "listenport", "80", "local port to listen")
+	flag.StringVar(&remoteAddr, "remoteaddr", "2.2.2.2:22", "remote address to forward")
+	flag.StringVar(&sourceAddr, "sourceaddr", "1.1.1.1", "source address to filter")
+	flag.StringVar(&listenAddr, "listenaddr", "172.19.0.2", "local address to listen")
+	flag.Parse()
 	l, err := lc.Listen(context.Background(), "tcp", listenAddr+":"+localPort)
 	if err != nil {
 		panic(err)
@@ -77,28 +77,26 @@ func main() {
 	fmt.Printf("TCP Server with PID: %d is running \n", pid)
 
 	for {
-        // 接受客户端连接
-        conn, err := l.Accept()
-        if err != nil {
-            break
-        }
-        fmt.Println("Accepted connection from", conn.RemoteAddr())
+		// 接受客户端连接
+		conn, err := l.Accept()
+		if err != nil {
+			break
+		}
+		fmt.Println("Accepted connection from", conn.RemoteAddr())
 
-        // 判断来源地址是否为1.1.1.1，如果是则转发流量，否则关闭连接
-        if strings.HasPrefix(conn.RemoteAddr().String(), sourceAddr) {
-            fmt.Println("Forwarding traffic to", remoteAddr)
-            // 创建远程连接套接字
-            rconn, err := net.Dial("tcp", remoteAddr)
-            handleError(err)
+		// 判断来源地址是否为1.1.1.1，如果是则转发流量，否则关闭连接
+		if strings.HasPrefix(conn.RemoteAddr().String(), sourceAddr) {
+			fmt.Println("Forwarding traffic to", remoteAddr)
+			// 创建远程连接套接字
+			rconn, err := net.Dial("tcp", remoteAddr)
+			handleError(err)
 
-            // 启动两个协程，分别转发两个方向的流量
-            go forward(conn, rconn)
-            go forward(rconn, conn)
-        } else {
-            fmt.Println("Closing connection from", conn.RemoteAddr())
-            conn.Close()
-        }
-    }
+			// 启动两个协程，分别转发两个方向的流量
+			go forward(conn, rconn)
+			go forward(rconn, conn)
+		} else {
+			fmt.Println("Closing connection from", conn.RemoteAddr())
+			conn.Close()
+		}
+	}
 }
-
-
